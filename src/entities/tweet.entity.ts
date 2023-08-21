140320f4407a8878b6ff082895f401224c51e6fa
= <=>
2023-08-21 13:07:38 -0300

    add: dtos & entities from tweet and user

---
+++ src/entities/tweet.entity.ts
@@ -0,0 +1,8 @@
+export class Tweet {
+    username: string;
+    tweet: string;
+    constructor(username:string, tweet:string) {
+      this.username = username;
+      this.tweet = tweet;
+    }
+  }
